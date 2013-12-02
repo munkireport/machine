ae5f099aadcf15bf9b1482228acdd5ca1a4b2573
Arjen van Bochoven <arjen@bochoven.net>
2013-12-02 20:16:31 +0100

    Moved machine to module

---
+++ machine_model.php
@@ -0,0 +1,79 @@
+<?php
+class Machine_model extends Model {
+
+	function __construct($serial='')
+	{
+		parent::__construct('id', 'machine'); //primary key, tablename
+		$this->rs['id'] = '';
+		$this->rs['serial_number'] = $serial; $this->rt['serial_number'] = 'VARCHAR(255) UNIQUE';
+		$this->rs['hostname'] = '';
+		$this->rs['machine_model'] = '';
+		$this->rs['machine_desc'] = '';
+		$this->rs['img_url'] = '';
+		$this->rs['current_processor_speed'] = '';
+		$this->rs['cpu_arch'] = '';
+		$this->rs['os_version'] = '';
+		$this->rs['physical_memory'] = 0;
+		$this->rs['platform_UUID'] = '';
+		$this->rs['number_processors'] = 0;
+		$this->rs['SMC_version_system'] = '';
+		$this->rs['boot_rom_version'] = '';
+		$this->rs['bus_speed'] = '';
+		$this->rs['computer_name'] = '';
+		$this->rs['l2_cache'] = '';
+		$this->rs['machine_name'] = '';
+		$this->rs['packages'] = '';	   
+		
+		// FIXME: add indexes
+
+		// Schema version, increment when creating a db migration
+		$this->schema_version = 1;
+
+		// Create table if it does not exist
+		$this->create_table();
+		
+		if ($serial)
+			$this->retrieve_one('serial_number=?', $serial);
+		
+		$this->serial = $serial;
+		  
+	}
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Process data sent by postflight
+	 *
+	 * @param string data
+	 * @author abn290
+	 **/
+	function process($plist)
+	{
+		echo "Machine: got data\n";
+		
+		require_once(APP_PATH . 'lib/CFPropertyList/CFPropertyList.php');
+		$parser = new CFPropertyList();
+		$parser->parse($plist, CFPropertyList::FORMAT_XML);
+		$mylist = $parser->toArray();
+		
+		// Remove serial_number from mylist, use the cleaned serial that was provided in the constructor.
+		unset($mylist['serial_number']);
+
+		// Set default computer_name
+		if( ! isset($mylist['computer_name']) OR trim($mylist['computer_name']) == '')
+		{
+			$mylist['computer_name'] = 'No name';
+		}
+
+		// Convert memory string (4 GB) to int
+		if( isset($mylist['physical_memory']))
+		{
+			$mylist['physical_memory'] = intval($mylist['physical_memory']);
+		}
+		
+		$this->timestamp = time();
+		$this->merge($mylist)->save();
+	}
+
+	
+}
