b2b15f1b4c94019935536081fdd0cb99238c6605
bochoven <arjen@bochoven.net>
2020-03-02 22:36:53 +0100

    New widgets and listing

--- views/new_clients_widget.php
+++ views/new_clients_widget.php
@@ -1,56 +1,15 @@
-<div class="col-lg-4 col-md-6">
+<?php 
 
-	<div class="panel panel-default" id="new-clients-widget">
-
-		<div class="panel-heading" data-container="body" data-i18n="[title]machine.new_clients.tooltip">
-
-			<div class="panel-title"><i class="fa fa-star-o"></i>
-			    <span data-i18n="machine.new_clients.title"></span>
-			    <span class="counter badge"></span>
-			    <list-link data-url="/show/listing/reportdata/clients"></list-link>
-			</div>
-
-		</div>
-
-		<div class="list-group scroll-box">
-			<span class="list-group-item"><span data-i18n="machine.new_clients.no_new_clients"></span></span>
-		</div>
-
-	</div><!-- /panel -->
-
-</div><!-- /col -->
-
-<script>
-$(document).on('appUpdate', function(){
-
-	$.getJSON( appUrl + '/module/machine/new_clients', function( data ) {
-
-		var scrollBox = $('#new-clients-widget .scroll-box').empty();
-
-		$.each(data, function(index, obj){
-
-			scrollBox
-				.append($('<a>')
-					.addClass('list-group-item')
-					.attr('href', appUrl + '/clients/detail/' + obj.serial_number)
-					.append(obj.computer_name)
-					.append($('<span>')
-						.addClass('pull-right')
-						.text(function(){
-								return moment(obj.reg_timestamp * 1000).fromNow();
-							})))
-
-		});
-
-		$('#new-clients-widget .counter').html(data.length);
-
-		if( ! data.length){
-			scrollBox
-				.append($('<span>')
-					.addClass('list-group-item')
-					.text(i18n.t('no_clients')))
-		}
-
-	});
-});
-</script>
+$this->viewWidget(
+[
+	"type" => "scrollbox",
+	"widget_id" => "new-clients-widget",
+	"api_url" => "/module/machine/new_clients",
+	"i18n_title" => 'machine.new_clients.title',
+	"icon" => "fa-star-o",
+	"listing_link" => "/show/listing/reportdata/clients",
+	"search_key" => "computer_name",
+	"i18n_tooltip" => "machine.new_clients.tooltip",
+	"badge" => "reg_timestamp",
+	"url_type" => "client_detail",
+]);
