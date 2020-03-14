3b15275680ff41c9a962a62f07da9a6476eb8af2
bochoven <arjen@bochoven.net>
2020-03-14 17:49:27 +0100

    Convert bargraph widgets to new template

--- views/os_widget.php
+++ views/os_widget.php
@@ -1,43 +1,13 @@
-<div class="col-md-6">
+<?php 
 
-	<div class="panel panel-default" id="os-widget">
-
-		<div class="panel-heading">
-
-			<h3 class="panel-title"><i class="fa fa-apple"></i>
-			    <span data-i18n="machine.os.title"></span>
-			    <list-link data-url="/show/listing/reportdata/clients"></list-link>
-			</h3>
-
-		</div>
-
-		<div class="panel-body">
-
-			<svg style="width:100%"></svg>
-
-		</div>
-
-	</div><!-- /panel -->
-
-</div><!-- /col-lg-4 -->
-
-<script>
-$(document).on('appReady', function(e, lang) {
-
-	var conf = {
-		url: appUrl + '/module/machine/os', // Url for json
-		widget: 'os-widget', // Widget id
-		elementClickCallback: function(e){
-			var label = mr.integerToVersion(e.data.label);
-			window.location.href = appUrl + '/show/listing/reportdata/clients#' + label;
-		},
-		labelModifier: function(label){
-			return mr.integerToVersion(label)
-		}
-	};
-
-	mr.addGraph(conf);
-
-});
-
-</script>
+$this->viewWidget(
+[
+	"type" => "bargraph",
+	"widget_id" => "os-widget",
+	"api_url" => "/module/machine/os",
+	"i18n_title" => 'machine.os.title',
+	"icon" => "fa-apple",
+	"listing_link" => "/show/listing/reportdata/clients",
+	"label_modifier" => "mr.integerToVersion(label)",
+	"search_component" => "mr.integerToVersion(e.data.label)",
+]);
