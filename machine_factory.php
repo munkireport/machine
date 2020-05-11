6fedf592d4a7da12c7aaa0f1cb4ddb54d1d00c32
Arjen van Bochoven <arjen@bochoven.net>
2020-05-11 23:42:44 +0200

    Use FakerDataStore for machines

--- machine_factory.php
+++ machine_factory.php
@@ -1,21 +1,7 @@
 <?php
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(Machine_model::class, function (Faker\Generator $faker) {
-    $machines = [
-        ['iMac', 'iMac9,1', 'iMac (20-inch, Mid 2009)'],
-        ['Macmini', 'Macmini7,1', 'Mac mini (Late 2014)'],
-        ['Macmini', 'Macmini5,1', 'Mac mini (Mid 2011)'],
-        ['MacBook Pro', 'MacBookPro12,1', 'MacBook Pro (Retina, 13-inch, Early 2015)'],
-        ['MacBook Pro', 'MacBookPro8,1', 'MacBook Pro (13-inch, Late 2011)'],
-        ['MacBook Pro', 'MacBookPro9,2', 'MacBook Pro (13-inch, Mid 2012)'],
-        ['Macmini', 'Macmini6,2', 'Mac mini (Late 2012)'],
-        ['MacBook', 'MacBook7,1', 'MacBook (13-inch, Mid 2010)'],
-        ['iMac', 'iMac14,2', 'iMac (27-inch, Late 2013)'],
-        ['iMac', 'iMac10,1', 'iMac (21.5-inch, Late 2009)'],
-        ['iMac', 'iMac14,4', 'iMac (21.5-inch, Mid 2014)'],
-        ['iMac', 'iMac12,1', 'iMac (21.5-inch, Mid 2011)'],
-        ['iMac', 'iMac16,2', 'iMac (21.5-inch, Late 2015)'],
-    ];
 
     $oses = [
         ['101206', '16G29'],
@@ -23,13 +9,13 @@ $factory->define(Machine_model::class, function (Faker\Generator $faker) {
         ['101503', '19D76'],
     ];
 
-    list($machine_name, $machine_model, $machine_desc) = $faker->randomElement($machines);
+    list($modelcode, $machine_name, $machine_model, $machine_desc) = FakerDataStore::get('machine_factory', 'machine');
     list($os_version, $build) = $faker->randomElement($oses);
 
     $computerName = $faker->firstName() . '\'s ' . $machine_name;
 
     return [
-        'hostname' => $computerName . '.local',
+        'hostname' => $faker->domainWord() . '.local',
         'machine_model' => $machine_model,
         'machine_desc' => $machine_desc,
         'img_url' => '',
