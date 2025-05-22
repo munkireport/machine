0b4907f8fb8d10be540e4e6b1a0cbd4b4c27ee3d
Alex Narvey <anarvey@precursor.ca>
2025-05-22 06:49:27 -0500

    Update machine_detail_widget1.php
    Removed reference to the )optional) ibridge module. The icon's color is therefore no longer specified and the generic icon is pulled instead.

--- views/machine_detail_widget1.php
+++ views/machine_detail_widget1.php
@@ -15,31 +15,6 @@
 </div>
 
 <script>
-let mac_color = ''; // Shared variable across both functions
-
-$(document).ready(function () {
-
-    $.getJSON(appUrl + '/module/ibridge/get_data/' + serialNumber)
-    .done(function (data) {
-		if (Array.isArray(data) && data.length > 0 && data[0] && data[0].device_color) {
-    		mac_color = data[0].device_color.toLowerCase().replace(/\s+/g, '');
-    		console.log("mac_color:", mac_color);
-    		$('.mac_color').text(mac_color);
-		} else {
-    		console.warn("iBridge data is empty or invalid:", data);
-    		mac_color = "";
-    	$('.mac_color').text('');
-		}        
-	loadMachineData();
-    })
-    .fail(function (jqXHR, textStatus, errorThrown) {
-        console.error("Failed to fetch iBridge data:", textStatus, errorThrown);
-        mac_color = "";
-        $('.mac_color').text('');
-        loadMachineData();
-    });
-    
-    
 function loadMachineData() {
     $.getJSON(appUrl + '/module/machine/report/' + serialNumber, function (data) {
         let mac_name = data.machine_name ? data.machine_name.replace(/ /g, '') : '';
@@ -50,14 +25,8 @@ function loadMachineData() {
             mac_name = "iMac";
         }
 
-        // Override for 13" 2022 Macbook Air Starlight color doesn't work
-        if (mac_color == "starlight" && mac_model == "Mac14,2"){
-           mac_color = "silver"
-        }
-
         console.log("mac_name:", mac_name);
         console.log("mac_model:", mac_model);
-        console.log("mac_color (used in URL):", mac_color);
 
         $('.mac_name').text(mac_name);
         $('.mac_model').text(mac_model);
@@ -65,28 +34,19 @@ function loadMachineData() {
         // Construct base image URL
         let imageUrl = "https://statici.icloud.com/fmipmobile/deviceImages-9.0/" +
             encodeURIComponent(mac_name) + "/" +
-            encodeURIComponent(mac_model);
-
-        // Append mac_color if it's valid and model is not iMacPro1,1
-        if (mac_model !== "iMacPro1,1" && mac_color && mac_color.trim()) {
-            imageUrl += "-" + encodeURIComponent(mac_color);
-        }
-        imageUrl += "/online-infobox__2x.png";
+            encodeURIComponent(mac_model) +
+            "/online-infobox__2x.png";
 
         $('#apple_mac_icon').attr('src', imageUrl);
-
-        // Update alt attribute dynamically
-        const altText = `Image of ${mac_name} model ${mac_model} color ${mac_color}`;
-        $('#apple_mac_icon').attr('alt', altText);
     });
 }
 
-    // Only call loadIbridgeData initially; it will call loadMachineData after mac_color is set
-    loadIbridgeData();
+$(document).ready(function () {
+    loadMachineData();
 
     $('.machine-refresh-desc').on('click', function (e) {
         e.preventDefault();
-        loadIbridgeData(); // Same logic for refresh
+        loadMachineData();
     });
 });
 </script>
